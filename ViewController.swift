07f4819e2db4aff59e70f27f2c35358e26fb0649
shawncrisp <ypsirc@gmail.com>
2016-06-17 15:44:03 -0400

    Start of Project

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleLogin
+//
+//  Created by Shawn Crisp on 2016-06-17.
+//  Copyright © 2016 Shawn Crisp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
